203d21797d69a8021c1970d2f95cc18d7900539a
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-12-17 14:38:27 -0500

    Initialization region

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import { Server } from "socket.io";
 import { createServer } from "http";
 import express, { static } from "express";
-import { Socket } from "./Socket.js";
+import { Socket } from "./Socket";
+//#region initialization
 const app = express();
 const http = createServer(app);
 const io = new Server(http)
 app.use(static("../client"))
+//#endregion
 
 io.on("connection", sock => {
 	new Socket(sock, io).init();
